ef3c62c3d78c7fe2973e4edc6199ae6a5365de48
waratake2000 <waratake.dev2000@gmail.com>
2023-07-07 17:29:57 +0900

    v1.4.0 intial commit

--- source/train_runner_production/auto_trainer.py
+++ source/train_runner_production/auto_trainer.py
@@ -14,11 +14,11 @@ def division_patterns(num):
 training_samples, _ = train_test_split(str(config.ANNOTATION_DATA),config.TEST_SPLIT)
 training_data_coutns = len(training_samples)
 
-for data_aug_factor in [2,5,10]:
+for data_aug_factor in [1,5,10]:
     train_data_len_divisors = division_patterns(int(training_data_coutns * data_aug_factor))
     for batch_size in train_data_len_divisors[0:6*data_aug_factor:data_aug_factor]:
         try:
-            # python3 train.py --EPOCHS 2000 --BATCH_SIZE 1 --LR 0.0001 --MODEL_FILE resnet18 --DATA_AUG_FAC
+            # python3 train.py --EPOCHS 2000 --BATCH_SIZE 400 --LR 0.0001 --MODEL_FILE resnet18 --DATA_AUG_FAC
             command = ["python3", "train.py", "--EPOCHS", "20000", "--BATCH_SIZE", f"{batch_size}", "--LR", "0.0001", "--MODEL_FILE", "resnet18", "--DATA_AUG_FAC", f"{data_aug_factor}"]
             subprocess.run(command)
         except:
